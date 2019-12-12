dcf35af26c71e1e14387a757d5ad77b18c379600
Genesis Pereira <profgenesislima@gmail.com>
2019-12-12 01:58:05 -0300

    initial commit for jsgp

---
+++ src/main/java/org/ntvru/jsgp/JsgpApplication.java
@@ -0,0 +1,13 @@
+package org.ntvru.jsgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsgpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsgpApplication.class, args);
+	}
+
+}
